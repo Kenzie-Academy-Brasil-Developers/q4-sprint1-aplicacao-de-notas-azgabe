3c23753b599c973bd2b7e2dd45b203e0897d40a8
Gabriel Azevedo <gmarquesazti@gmail.com>
2022-03-15 16:44:11 -0300

    feat: patch note route

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let database = [];
 
 const validateCpf = (req, res, next) => {
   const { cpf } = req.body;
+
   const cpfList = database.map((user) => user.cpf);
 
   if (cpfList.includes(cpf)) {
@@ -19,6 +20,7 @@ const validateCpf = (req, res, next) => {
 
 const verifyUser = (req, res, next) => {
   const { cpf } = req.params;
+
   const cpfList = database.map((user) => user.cpf);
 
   if (!cpfList.includes(cpf)) {
@@ -31,6 +33,7 @@ const verifyUser = (req, res, next) => {
 const verifyNote = (req, res, next) => {
   const { cpf, id } = req.params;
   const { notes } = database.find((user) => user.cpf === cpf);
+
   const idList = notes.map((note) => note.id);
 
   if (!idList.includes(id)) {
@@ -42,6 +45,7 @@ const verifyNote = (req, res, next) => {
 
 app.post("/users", validateCpf, (req, res) => {
   const { name, cpf } = req.body;
+
   const user = { id: uuidv4(), name, cpf, notes: [] };
 
   database.push(user);
@@ -78,6 +82,7 @@ app.delete("/users/:cpf", verifyUser, (req, res) => {
 app.post("/users/:cpf/notes", verifyUser, (req, res) => {
   const { cpf: routeCpf } = req.params;
   const { title, content } = req.body;
+
   const now = new Date();
 
   const newNote = { id: uuidv4(), created_at: now, title, content };
@@ -95,9 +100,34 @@ app.post("/users/:cpf/notes", verifyUser, (req, res) => {
 
 app.get("/users/:cpf/notes", verifyUser, (req, res) => {
   const { cpf: routeCpf } = req.params;
+
   const { notes } = database.find((user) => user.cpf === routeCpf);
 
   res.status(200).json(notes);
 });
+
+app.patch(
+  "/users/:cpf/notes/:id",
+  verifyUser,
+  verifyNote,
+  validateCpf,
+  (req, res) => {
+    const { cpf: routeCpf, id: routeId } = req.params;
+    const { title: newTitle, content: newContent } = req.body;
+    const { notes } = database.find((user) => user.cpf === routeCpf);
+
+    const now = new Date();
+
+    notes.forEach((note) => {
+      if (note.id === routeId) {
+        note.title = newTitle !== undefined ? newTitle : note.title;
+        note.content = newContent !== undefined ? newContent : note.content;
+        note.updated_at = now;
+
+        res.status(200).json(note);
+      }
+    });
+  }
+);
 
 app.listen(3000, () => null);
