1ab08880d9b74591b38360f78244caba1f2f878e
Gabriel Azevedo <gmarquesazti@gmail.com>
2022-03-15 13:37:25 -0300

    feat: post user route

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 app.use(express.json());
 
-let database = [];
+const database = [];
 
 const verifyCpfAvailability = (req, res, next) => {
   const { cpf } = req.body;
   const cpfList = database.map((user) => user.cpf);
 
-  if (cpfList.includes(cpf)) return { error: "user already exists" };
+  if (cpfList.includes(cpf)) {
+    res.statusCode = 422;
+    return res.json({ error: "user already exists" });
+  }
 
   return next();
 };
@@ -18,7 +22,10 @@ const verifyUserExistence = (req, res, next) => {
   const { cpf } = req.params;
   const cpfList = database.map((user) => user.cpf);
 
-  if (!cpfList.includes(cpf)) return { error: "user is not registered" };
+  if (!cpfList.includes(cpf)) {
+    res.statusCode = 404;
+    return res.json({ error: "user is not registered" });
+  }
 
   return next();
 };
@@ -28,9 +35,22 @@ const verifyNoteExistence = (req, res, next) => {
   const { notes } = database.find((user) => user.cpf === cpf);
   const idList = notes.map((note) => note.id);
 
-  if (!idList.includes(id)) return { error: "note is not registered" };
+  if (!idList.includes(id)) {
+    res.statusCode = 404;
+    return res.json({ error: "note is not registered" });
+  }
 
   return next();
 };
+
+app.post("/users", verifyCpfAvailability, (req, res) => {
+  const { name, cpf } = req.body;
+  const user = { id: uuidv4(), name, cpf, notes: [] };
+
+  database.push(user);
+
+  res.statusCode = 201;
+  res.json(user);
+});
 
 app.listen(3000, () => "Aplicação rodando em http://localhost:3000");
